0c85a555d6b64e1f25e78989edaf11b3517566c1
SollWar <63407355+SollWar@users.noreply.github.com>
2020-04-09 17:46:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drevo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bt = new btree();
+            bt.Insert(8);
+
+            bt.Insert(4);
+            bt.Insert(12);
+
+            bt.Insert(2);
+            bt.Insert(6);
+            bt.Insert(10);
+            bt.Insert(14);
+
+            bt.Insert(1);
+            bt.Insert(3);
+            bt.Insert(5);
+            bt.Insert(7);
+            bt.Insert(9);
+            bt.Insert(11);
+            bt.Insert(13);
+            bt.Insert(15);
+            bt.Print();
+            //Console.WriteLine(bt.root.left.right.val);
+
+
+        }
+    }
+}
---
+++ btree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drevo
+{
+    public class btree
+    {
+        public Node root;
+
+        public void Insert(int x)
+        {
+            if (root == null)
+                root = new Node(x);
+            else
+                insert(x, root);
+        }
+
+        private void insert(int x, Node leaf)
+        {
+            if (x < leaf.val)
+            {
+                if (leaf.left == null)
+                    leaf.left = new Node(x);
+                else
+                    insert(x, leaf.left);
+            }
+            else
+            {
+                if (leaf.right == null)
+                    leaf.right = new Node(x);
+                else
+                    insert(x, leaf.right);
+            }
+        }
+
+        public void Print()
+        {
+            if (root != null)
+                print(root, 0);
+        }
+
+        private void print(Node n, int step)
+        {
+
+            if (n.right != null)
+                print(n.right, step + 1);
+
+            for (int i = 0; i != step; i++)
+                Console.Write("  ");
+            Console.WriteLine(n.val);
+
+            if (n.left != null)
+                print(n.left, step + 1);
+        }
+
+    }
+}
