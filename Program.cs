83c5f7df08a502605dc9d768a343b955464e29c8
SollWar <63407355+SollWar@users.noreply.github.com>
2020-04-16 17:33:02 +0300

    16.04(2)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,40 @@ namespace Drevo
 {
     class Program
     {
+        //delegate int tr(int x);
+        //delegate int re(int x, int y);
+        
         static void Main(string[] args)
         {
+            /*
+            tr sq = x => x * x;
+            Console.WriteLine(sq(3));
+            Console.WriteLine(sq.Invoke(3));
+
+            re sq2 = (a, b) => a + b;
+            Console.WriteLine(sq2(3, 4));
+
+            System.Action<int, int> ss = null;
+            int k = 0;
+            ss = (x, y) => { k++; };
+            ss(3, 4);
+            Console.WriteLine(k);
+
+            System.Func<int, int, int> f1 = null;
+            f1 = (a, b) => {
+                if (a == 5)
+                    return a*2 + b;
+                else
+                    return a + b;
+            };
+            Console.WriteLine(f1(5, 6));
+
+            var lst = new List<int>() { 1, 2, 3, 4 };
+            lst.ForEach(x => Console.Write(x + "\t"));
+            Console.WriteLine("\n");
+
+            */
+
             var bt = new btree();
 
             bt.Insert(5);
@@ -22,9 +54,6 @@ namespace Drevo
 
             bt.Print1();
             Console.ReadKey();
-            //Console.WriteLine(bt.root.left.right.val);
-
-
         }
     }
 }
